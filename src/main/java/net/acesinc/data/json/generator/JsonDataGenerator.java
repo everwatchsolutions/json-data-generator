4e11d8c10da3cb288b8f3fa78837f2f48180f403
Betselot Hailu <betselothailu@gmail.com>
2016-07-07 15:58:45 +0200

    fix misspelling

--- src/main/java/net/acesinc/data/json/generator/JsonDataGenerator.java
+++ src/main/java/net/acesinc/data/json/generator/JsonDataGenerator.java
@@ -87,7 +87,7 @@ public class JsonDataGenerator {
     }
 
     public static void main(String[] args) {
-        String simConfig = "defualtSimConfig.json";
+        String simConfig = "defaultSimConfig.json";
         if (args.length > 0) {
             simConfig = args[0];
             log.info("Overriding Simulation Config file from command line to use [ " + simConfig + " ]");
