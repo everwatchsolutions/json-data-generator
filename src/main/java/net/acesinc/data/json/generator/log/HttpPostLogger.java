5433859de1f9ef4ae15340a84a09b1eca67f50fc
Casey Crites <crites.casey@gmail.com>
2021-10-26 11:46:05 -0700

    We respond with 200's, not 201's

--- src/main/java/net/acesinc/data/json/generator/log/HttpPostLogger.java
+++ src/main/java/net/acesinc/data/json/generator/log/HttpPostLogger.java
@@ -74,7 +74,7 @@ public class HttpPostLogger implements EventLogger {
             CloseableHttpResponse response = null;
             try {
                 response = httpClient.execute(request);
-                if (response.getStatusLine().getStatusCode() != 201) {
+                if (response.getStatusLine().getStatusCode() != 200) {
                     log.warn("Non-201 response code: " + response.getStatusLine().getStatusCode());
                     log.warn("Response: " + EntityUtils.toString(response.getEntity()));
                 }
